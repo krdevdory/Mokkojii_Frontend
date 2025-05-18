5423d488c0035d8eaac87ff9d1853b2f9e4d5e9e
dev <dev@devui-MacBookPro.local>
2025-05-19 01:35:02 +0900

    navbar 초기 세팅

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
 import { useState } from 'react'
 import './App.css'
+import NavBar from "./components/side_nav";
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div className='flex h-screen justify-center items-center'>
+    <div className='flex'>
+      <NavBar />
+      <div className='flex w-full h-screen justify-center items-center'>
         <p className='font-bold text-xl'>TailwindCSS Test</p>
       </div>
-    </>
+    </div>
   )
 }
 
